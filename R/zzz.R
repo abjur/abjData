7c7e6f297cfa71264aee453cf73eecf2107eb99d
Julio Trecenti <julio.trecenti@gmail.com>
2017-01-09 21:02:21 -0200

    global vars

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+globalVariables(c("%>%", ".", "Name", "geom", "geometry", "id",
+                  "id_objeto", "latitude", "longitude", "mun_uf",
+                  "municipio", "setNames", "uf", "unzip", "v1", "v2"))
