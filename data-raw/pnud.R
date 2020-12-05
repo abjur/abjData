e1f2fbdc1903cde0f25be6d649ffba1df54fe1ba
Julio Trecenti <julio.trecenti@gmail.com>
2020-12-05 11:46:11 -0300

    adicionando base de municipios

---
+++ data-raw/pnud.R
@@ -0,0 +1,22 @@
+## code to prepare `pnud` dataset goes here
+
+## TODO criar codigo reprodutivel
+
+
+aux_join <- pnud_muni %>%
+  select(ano, uf, municipio, muni_id = codmun7) %>%
+  mutate(across(.fns = as.character)) %>%
+  left_join(distinct(muni, uf_id, uf_sigla), c("uf" = "uf_id")) %>%
+  select(-uf)
+
+# adicionando codigo municipio na pnud_min
+pnud_min <- pnud_min %>%
+  mutate(ano = as.character(ano)) %>%
+  inner_join(aux_join, c("uf" = "uf_sigla", "muni" = "municipio", "ano")) %>%
+  relocate(muni_id, .before = muni) %>%
+  rename(muni_nm = muni, uf_sigla = uf, regiao_nm = regiao)
+
+# usethis::use_data(pnud_siglas, overwrite = TRUE)
+# usethis::use_data(pnud_uf, overwrite = TRUE)
+# usethis::use_data(pnud_muni, overwrite = TRUE)
+usethis::use_data(pnud_min, overwrite = TRUE)
